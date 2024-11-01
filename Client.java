bf1afd0c4082137e403adf7054f3908b88fbc7a8
aulonaramosaj <aulona.ramosaj@student.uni-pr.edu>
2024-11-01 23:49:35 +0100

    Update  Client

--- Client.java
+++ Client.java
@@ -4,4 +4,24 @@ public class Client {
     private Socket socket = null;
     private DataInputStream input = null;
     private DataOutputStream out = null;
+
+    public Client(String address, int port){
+        try{
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            input = new DataInputStream(System.in);
+
+            out = new DataOutputStream(
+                    socket.getOutputStream());
+        }
+        catch(UnknownHostException u) {
+            System.out.println(u);
+            return;
+        }
+        catch (IOException i) {
+            System.out.println(i);
+            return;
+        }
+    }
 }
