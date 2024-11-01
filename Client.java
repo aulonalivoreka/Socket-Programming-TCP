942f6b7d7b0c0c3d53f3bee2738208838327993f
aulonaramosaj <aulona.ramosaj@student.uni-pr.edu>
2024-11-01 23:45:33 +0100

    Update  Client

--- Client.java
+++ Client.java
@@ -1,2 +1,7 @@
+import java.io.*;
+import java.net.*;
 public class Client {
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
 }
