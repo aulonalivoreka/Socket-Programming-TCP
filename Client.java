d11dc1992eb34e8b742ea85d1f350fb3a925396d
arlindabeqiraj <arlinda.beqiraj1@student.uni-pr.edu>
2024-11-02 15:11:10 +0100

    update client

--- Client.java
+++ Client.java
@@ -35,5 +35,21 @@ public class Client {
             }
         }
 
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Client client = new Client("127.0.0.1", 5000);
     }
 }
+
+
+
