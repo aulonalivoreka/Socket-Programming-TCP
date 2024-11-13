2f267887a9cb08005f7cdf0a462e6d27ea75745a
aulonaramosaj <aulona.ramosaj@student.uni-pr.edu>
2024-11-13 11:32:13 +0100

    Update Client

--- Client.java
+++ Client.java
@@ -1,6 +1,9 @@
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.net.*;
+import java.util.Base64;
+
 public class Client {
     private static final String SECRET_KEY_STRING = "YOUR_BASE64_SECRET_KEY";
     private Socket clientSocket;
@@ -9,25 +12,27 @@ public class Client {
     private BufferedReader userInput;
     private SecretKey secretKey;
 
-    public Client(String address, int port){
-        try{
-            socket = new Socket(address, port);
-            System.out.println("Connected");
+    public Client(String serverIp, int port, boolean fullAccess) {
+        try {
+            byte[] decodedKey = Base64.getDecoder().decode(SECRET_KEY_STRING);
+            secretKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
 
-            input = new DataInputStream(System.in);
+            clientSocket = new Socket(serverIp, port);
+            System.out.println("Connected to server at " + serverIp + ":" + port);
 
-            out = new DataOutputStream(
-                    socket.getOutputStream());
-        }
-        catch(UnknownHostException u) {
-            System.out.println(u);
-            return;
-        }
-        catch (IOException i) {
-            System.out.println(i);
-            return;
-        }
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            userInput = new BufferedReader(new InputStreamReader(System.in));
 
+            if (fullAccess) requestFullAccess();
+            else requestReadAccess();
+
+            readServerResponses();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
         String line = "";
         while (!line.equals("Over")) {
             try {
